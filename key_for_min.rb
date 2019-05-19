bc3f369d139a18d44d675bf86752327bcacd53f7
Tori Sigler <mssigler@gmail.com>
2019-05-19 16:48:28 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+	current_lowest = Float::INFINITY
+	current_key = nil
+
+	if name_hash == {}
+		return nil			# might not need .. 
+	else name_hash.each do |name, val|
+		#current_lowest = val
+		if val < current_lowest
+			current_lowest = val
+			current_key = name
+		end
+	end
+	end
+
+	return current_key
 
 end
